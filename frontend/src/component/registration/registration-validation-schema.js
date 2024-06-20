cbcba95d489addfb349dd0a85d2a05f33e1ecffa
opifex <opifexM@outlook.com>
2024-06-20 11:46:12 -0400

    Update minimum password length in registration validation schema

--- frontend/src/component/registration/registration-validation-schema.js
+++ frontend/src/component/registration/registration-validation-schema.js
@@ -7,7 +7,7 @@ export const registrationValidationSchema = (t) => Yup.object({
     .max(20, t('user.validation.length', { min: 3, max: 20 }))
     .required(t('user.validation.required')),
   password: Yup.string()
-    .min(6, t('user.validation.min', { min: 3 }))
+    .min(6, t('user.validation.min', { min: 6 }))
     .required(t('user.validation.required')),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password'), null], t('user.validation.passwordNotMatch'))
