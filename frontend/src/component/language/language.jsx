110bf0118e69f3dd1f9358690a0bd442ecdf01a3
opifex <opifexM@outlook.com>
2024-06-21 14:09:38 -0400

    Add language selection to dropdown in frontend and update localization files

--- frontend/src/component/language/language.jsx
+++ frontend/src/component/language/language.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 // eslint-disable-next-line import/prefer-default-export
 export const Language = () => {
   const { i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
@@ -10,12 +11,22 @@ export const Language = () => {
 
   return (
     <div className="dropdown">
-      <button className="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-        Language
+      <button
+        className="btn btn-link dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        {t('language')}
       </button>
       <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><button className="dropdown-item" type="button" onClick={() => changeLanguage('en')}>EN</button></li>
-        <li><button className="dropdown-item" type="button" onClick={() => changeLanguage('ru')}>RU</button></li>
+        <li>
+          <button className="dropdown-item" type="button" onClick={() => changeLanguage('en')}>EN</button>
+        </li>
+        <li>
+          <button className="dropdown-item" type="button" onClick={() => changeLanguage('ru')}>RU</button>
+        </li>
       </ul>
     </div>
   );
